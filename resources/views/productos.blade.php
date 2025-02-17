a6b7282055975643ca26991d2053d66e9e414417
gamp81 <gonzalomoncadapaz@gmail.com>
2025-02-16 23:51:52 -0500

    estilos tailwind productos y servicios

--- resources/views/productos.blade.php
+++ resources/views/productos.blade.php
@@ -1,42 +1,40 @@
 @extends('layouts.app')
 
 @section('content')
-<section id="service" class="service2 sections lightbg">
-
-            <div class="container">
+<section>
+        <div class="container">
                 <div class="row">
                     <h4 style="text-align:center;  "class="text-2xl text-black font-bold">PRODUCTOS ESTRELLAS</h4>
-					<div class="pricing col-md-12">
-						<div class="col-md-5">
-							<div class="price-box to-animate-2">
+					<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6 p-6">
+						<div class="bg-white p-6 rounded-lg shadow-md text-center">
+							
 								<h2 style="text-align:center;" class="text-2xl text-black font-bold ">Syslab</h2>
 								<a href="/syslab">
-                                <div class="price"><img src="/images1/syslab1.png" alt="" /></div></a>
+                                <div class="w-100 h-100 overflow-hidden"><img src="/images1/syslab1.png" class="w-full h-full object-cover" /></div></a>
 								<div class="text-1xl text-black text-left py-4 ">
-							            <p>Sistema de laboratori para gestionar laboratorios clinicos, resultados en linea con codigo QR.</p>
+							            <p>Sistema informático para gestionar laboratorios clínicos, resultados en linea con codigo QR.</p>
                                    
 								</div>
 								<div class="features_buttom">
-                                    <a href="/contactanos" class="btn btn-primary">Solicitar Información</a>
+                                    <a href="/contactanos" class="mt-4 inline-block bg-blue-600 text-white py-2 px-4 rounded">Solicitar Información</a>
 								
                                 </div>
-							</div>
+							
 						</div>
-                        <div class="col-md-2">
-                        </div>
-                        <div class="col-md-5">
-							<div class="price-box to-animate-2">
+                     
+                        <div class="bg-white p-6 rounded-lg shadow-md text-center">
+							
 							<h2 style="text-align:center;" class="text-2xl text-black font-bold ">Facturacion Electronica</h2>
                                 <a href="http://facturandoec.gmiti.com/">
-                                <div class="price"><img src="/images/facturando.png" alt="" /></div>
+                                <div class="w-100 h-100 overflow-hidden"><img src="/images/facturando.png" class="w-full h-full object-cover" /></div>
 								<div class="text-1xl text-black text-left py-4 ">
 									<p>Sistema de facturacion electronica para todo negocio.</p>
 								</div>
 								<br>
 								<div class="features_buttom">
-                                    <a href="/contactanos" class="btn btn-primary">Solicitar Información</a>
+                                    <a href="/contactanos" class="mt-4 inline-block bg-blue-600 text-white py-2 px-4 rounded">Solicitar Información</a>
                                 </div>	
-							</div>
+							
 						</div>
 					</div>
 				</div>
--- resources/views/servicios.blade.php
+++ resources/views/servicios.blade.php
@@ -4,57 +4,54 @@
 <section id="service" class="service2 sections lightbg">
 
             <div class="container">
-               
-
-                <div class="row">
-                   
+                              
                     <div class="main_service2 ">
-                        <div class="head_title text-center">
+                        <div class="head_title text-center py-10">
                             <h4 style="text-align:center;  "class="text-2xl text-black font-bold">SERVICIOS INFORMATICOS</h4>
-                            <p>Los servicios que ofrecemos son para toda empresa, negocio o domicilio, contamos con el personal calificado siempre en linea para una pronta respuesta.</p>
+                            <p class="py-5">Los servicios que ofrecemos son para toda empresa, negocio o domicilio, contamos con el personal calificado siempre en linea para una pronta respuesta.</p>
                         </div>
 
-                        <div class="service_content">
-                            <div class="col-md-6 col-sm-6">
-                                <div class="single_service2">
-                                    <div class="single_service_left">
-                                        <img src="/images/voip.png" alt="" />
+                        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
+                            <div class="bg-white p-6 rounded-lg text-center">
+                                <div class="grid grid-cols-2">
+                                    <div class="my-4 text-black text-6xl mx-auto">
+                                        <img src="/images/voip.png" class="w-42 h-42" />
                                     </div>
-                                    <div class="single_service_right">
-                                        <h2>Telefonia IP</h2>
+                                    <div class="text-left py-5">
+                                        <h2 class="text-2xl font-bold text-black">Telefonia IP</h2>
                                         <p>Proyectos grandes y pequeños, trabajamos con varias marcas muy reconocidas, centrales IP, PBX, Extensiones remotas.</p>
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-md-6 col-sm-6">
-                                <div class="single_service2">
-                                    <div class="single_service_left">
-                                       <img src="/images/lan.png" alt="" />
+                            <div class="bg-white p-6 rounded-lg text-center">
+                                <div class="grid grid-cols-2">
+                                    <div class="my-4 text-black text-6xl mx-auto">
+                                       <img src="/images/lan.png" class="w-42 h-42"/>
                                     </div>
-                                    <div class="single_service_right">
-                                        <h2>Redes Informáticas</h2>
+                                    <div class="text-left py-5">
+                                        <h2 class="text-2xl font-bold text-black">Redes Informáticas</h2>
                                         <p>Asesoramos a nuestros clientes, planificamos y desarrollamos los proyectos; cable estructurado, fibra óptica, redes inalámbricas o específicas, siendo partners de los principales fabricantes y desarrolladores.</p>
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-md-6 col-sm-6">
-                                <div class="single_service2">
-                                    <div class="single_service_left">
-                                      <img src="/images/soporte.png" alt="" />
+                            <div class="bg-white p-6 rounded-lg text-center">
+                                <div class="grid grid-cols-2">
+                                    <div class="my-4 text-black text-6xl mx-auto">
+                                      <img src="/images/soporte.png" class="w-45 h-45" />
                                     </div>
-                                    <div class="single_service_right">
-                                        <h2>Soporte IT</h2>
+                                    <div class="text-left py-5">
+                                        <h2 class="text-2xl font-bold text-black">Soporte IT</h2>
                                         <p>Consultoria IT, mantenimiento preventivo y correctivos de Hardware, Ofrecemos servicios de mantenimiento informático para clientes ubicados en la Provincia de El Oro</p>
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-md-6 col-sm-6">
-                                <div class="single_service2">
-                                    <div class="single_service_left">
-                                        <img src="/images/dev2.png" alt="" />
+                            <div class="bg-white p-6 rounded-lg text-center">
+                                    <div class="grid grid-cols-2">
+                                    <div class="my-4 text-black text-6xl mx-auto">
+                                        <img src="/images/dev2.png" class="w-40 h-40" />
                                     </div>
-                                    <div class="single_service_right">
-                                        <h2>Fabrica de Software</h2>
+                                    <div class="text-left py-5">
+                                        <h2 class="text-2xl font-bold text-black">Fabrica de Software</h2>
                                         <p>Competir es duro. Con un desarrollo software, podrá llegar a más clientes, reducir costes en operaciones, agilizar su administración...
                                             Explíquenos su proyecto: le hablaremos sobre viabilidad, financiación y costes en la primera visita.</p>
                                     </div>
@@ -62,7 +59,7 @@
                             </div>
                         </div>
                     </div>
-                </div>
+                
             </div>
         </section><!-- End of Service2 Section -->	
         @stop
